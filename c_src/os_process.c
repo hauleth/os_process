9b92afb4b149771ceac848e8b524a7e5d8655c82
Łukasz Niemier <lukasz@niemier.pl>
2020-03-21 12:31:15 +0100

    fix: remove unneeded `size` variable

--- c_src/os_process.c
+++ c_src/os_process.c
@@ -6,12 +6,11 @@ static void add(ErlNifEnv *env, ERL_NIF_TERM *map, struct Entry entry) {
   if (!enif_make_existing_atom(env, entry.key, &atom, ERL_NIF_LATIN1))
     atom = enif_make_atom(env, entry.key);
 
-  enif_make_map_put(env, *map, atom, (entry.gen)(env), map);
+  enif_make_map_put(env, *map, atom, entry.gen(env), map);
 }
 
 static ERL_NIF_TERM process_info_nif(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]) {
   ERL_NIF_TERM map = enif_make_new_map(env);
-  size_t size = 0;
 
   for (size_t i = 0; i < process_info_functions_size; ++i) {
     add(env, &map, process_info_functions[i]);
